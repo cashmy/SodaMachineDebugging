1390ff7859d01e93a8627524b103e44b6c625068
cashmy <cmyers880@gmail.com>
2021-03-25 11:48:13 -0500

    Corrected object comparison with a variable to object.variable == variable: can to can.name. Corrected argument passing to method calculate_transaction from variable selected_soda_name to object selected_soda

--- dCC_Python_SodaMachine/soda_machine.py
+++ dCC_Python_SodaMachine/soda_machine.py
@@ -38,17 +38,17 @@ class SodaMachine:
 
         selected_soda_name = user_interface.soda_selection(self.inventory)
 
-        # selected_soda = self.get_inventory_soda(selected_soda_name)
+        selected_soda = self.get_inventory_soda(selected_soda_name)
 
         customer_payment = customer.gather_coins_from_wallet(selected_soda_name)
 
-        self.calculate_transaction(customer_payment, selected_soda_name, customer)
+        self.calculate_transaction(customer_payment, selected_soda, customer)
 
         user_interface.output_text("Transaction complete")
 
     def calculate_transaction(self, customer_payment, selected_soda, customer):
         total_payment_value = self.calculate_coin_value(customer_payment)
-        if total_payment_value < selected_soda.price:
+        if total_payment_value < selected_soda.price:                                               # *** Error
             change_value = self.determine_change_value(total_payment_value, selected_soda.price)
             customer_change = self.gather_change_from_register(change_value)
             if customer_change is None:
@@ -125,7 +125,7 @@ class SodaMachine:
     def get_inventory_soda(self, selected_soda_name):
         """Returns the first instance of a can whose name matches the selected_soda_name parameter"""
         for can in self.inventory:
-            if can == selected_soda_name:
+            if can.name == selected_soda_name:
                 self.inventory.remove(can)
                 return can
         return None
