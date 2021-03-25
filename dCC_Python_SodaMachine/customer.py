15c76b1841332e671f7abad39282d1f19d9ca861
cashmy <cmyers880@gmail.com>
2021-03-25 13:12:19 -0500

    Correct injected parameter name in function end_message: soda to soda_name

--- dCC_Python_SodaMachine/customer.py
+++ dCC_Python_SodaMachine/customer.py
@@ -60,10 +60,10 @@ class Customer:
         total_value = round(total_value, 2)
         user_interface.display_customer_wallet_info(coins_quantity, total_value)
 
-    def check_backpack():
+    def check_backpack(self):
         """Will display the cans contained in purchased_cans list in backpack"""
-        if backpack.purchased_cans.length > 0:
+        if len(self.backpack.purchased_cans) <= 0:
             user_interface.output_text("You have no cans in your backpack")
         else:
-            for can in backpack.purchased_cans:
+            for can in self.backpack.purchased_cans:
                 user_interface.output_text(can.name)
